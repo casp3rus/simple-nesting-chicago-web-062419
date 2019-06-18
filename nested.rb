da3a269bed2286d5dcc9e57784a92df8c5c62bee
Adrian Popan <adrian.popan@gmail.com>
2019-06-18 02:28:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = [:alans_new_info]
      
 end
 
@@ -124,6 +124,6 @@ def adding_to_dennis
         }
      }
 languages_array = programmer_hash[:dennis_ritchie][:languages]
-adding_to_dennis = adding_to_dennis.push("Assembly")
+languages_array = languages_array.push("Assembly")
 
 end
