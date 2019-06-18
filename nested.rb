28afa84a60796992f3eed7c9aeb2d828da930530
Adrian Popan <adrian.popan@gmail.com>
2019-06-18 02:25:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-adding_to_dennis = programmer_hash[:dennis_ritchie][:languages]
+languages_array = programmer_hash[:dennis_ritchie][:languages]
 adding_to_dennis = adding_to_dennis.push("Assembly")
 
 end
